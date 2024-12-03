4ca9a217a0840659db6d2bafa3f2aeef480ec143
charan <charan.vangara34@gmail.com>
2024-12-03 11:05:41 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+import './App.css'
+import MainPage from './shoppingfolder/pages/Mainpage'
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
